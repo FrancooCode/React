d1fdac95415bac1dd5bad412f62c026e09c85621
franco <francoenina79@gmail.com>
2022-09-24 23:49:14 -0300

    primera entrega

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ItemListContainer from "./ItemListContainer/ItemListContainer";
+import NavBar from "./componets/NavBar/NavBar";
+
+
+function App() {
+  return (
+    <>
+      <NavBar/>
+      <ItemListContainer 
+      greeting= "Bienvenido a BlendHouse"/>
+    </>
+  );
+}
+
+export default App;
